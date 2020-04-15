2ab98aabe0d75f1a924506832bb04a0b1215906a
Ben Edwards <hello@benedfit.com>
2020-04-15 23:35:20 +0100

    feat(video): adds video file format support
    closes #92

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const withSitemap = require('@newhighsco/next-plugin-sitemap')
 const withRobots = require('@newhighsco/next-plugin-robots')
 const withSvgr = require('@newhighsco/next-plugin-svgr')
 const withFonts = require('next-fonts')
+const withVideos = require('next-videos')
 const withCssOptions = require('./src/plugins/css-options')
 
 const nextConfig = {
@@ -49,6 +50,7 @@ module.exports = withPlugins(
     ],
     [withCssOptions, { cssModulesOptions: { mode: 'local' } }],
     [withFonts],
+    [withVideos],
     [withSitemap, { sitemap: { hostname: nextConfig.env.SITE_URL } }],
     [
       withRobots,
