d4bb180e99b6ef68c6371e41b5d3bf9c8a402e02
Ben Edwards <hello@benedfit.com>
2020-05-02 03:56:32 +0100

    chore(footer): fixes alignment

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,26 +5,37 @@ import {
   Grid,
   GridItem,
   Icon,
+  List,
   SmartLink
 } from '@newhighsco/chipset'
 import icons from '../../images/icons'
 import { homepage, version } from '../../../package.json'
 
+import styles from './styles.module.scss'
+
 const GithubSvg = icons('github')
 
 const Footer = () => (
   <ContentContainer as="footer" role="contentinfo" fullWidth gutter>
-    <Grid>
+    <Grid valign="middle">
       <GridItem sizes={['one-half']}>
         <CreditLockup />
       </GridItem>
       <GridItem sizes={['one-half']} align="right">
-        <SmartLink href={homepage}>
-          v{version}{' '}
-          <Icon width={24}>
-            <GithubSvg />
-          </Icon>
-        </SmartLink>
+        <List inline>
+          <li>
+            <SmartLink href={homepage} className={styles.link}>
+              v{version}
+            </SmartLink>
+          </li>
+          <li>
+            <SmartLink href={homepage} className={styles.link}>
+              <Icon theme={{ wrapper: styles.icon }}>
+                <GithubSvg />
+              </Icon>
+            </SmartLink>
+          </li>
+        </List>
       </GridItem>
     </Grid>
   </ContentContainer>
