5750fb31df017462c0c7c265debc1e1410fe91ee
Ben Edwards <hello@benedfit.com>
2020-05-02 02:57:07 +0100

    chore: adds version number to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-import { ContentContainer, CreditLockup } from '@newhighsco/chipset'
+import {
+  ContentContainer,
+  CreditLockup,
+  Grid,
+  GridItem
+} from '@newhighsco/chipset'
+import { version } from '../../../package.json'
 
 const Footer = () => (
   <ContentContainer as="footer" role="contentinfo" fullWidth gutter>
-    <CreditLockup />
+    <Grid>
+      <GridItem sizes={['one-half']}>
+        <CreditLockup />
+      </GridItem>
+      <GridItem sizes={['one-half']} align="right">
+        v{version}
+      </GridItem>
+    </Grid>
   </ContentContainer>
 )
 
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,7 +1,9 @@
 import ContentContainer from '../components/ContentContainer/theme.module.scss'
+import Grid from '../components/Grid/theme.module.scss'
 import SiteContainer from '../components/SiteContainer/theme.module.scss'
 
 export default {
   ContentContainer,
+  Grid,
   SiteContainer
 }
