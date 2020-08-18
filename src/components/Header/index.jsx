65c8a34ec097650e6a7cee58688ea2c7cc0d9124
Ben Edwards <hello@benedfit.com>
2020-08-18 01:06:28 +0100

    fix(seo): remove top of page landmark

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import {
-  ContentContainer,
-  Icon,
-  Landmark,
-  SmartLink
-} from '@newhighsco/chipset'
+import { ContentContainer, Icon, SmartLink } from '@newhighsco/chipset'
 import { config } from '../../../site.config'
 
 import theme from './theme.module.scss'
@@ -14,8 +9,7 @@ import { ReactComponent as LogoSvg } from '../../images/logo.url.svg'
 
 const Header = () => (
   <>
-    <Landmark id="top">Top of page</Landmark>
-    <ContentContainer as="header" role="banner" gutter theme={theme}>
+    <ContentContainer id="top" as="header" role="banner" gutter theme={theme}>
       <Link href="/" passHref>
         <SmartLink>
           <Icon alt={config.name} theme={{ wrapper: styles.logo }}>
