61e7d3315f5cdb84e2a4af2c774ec03fafb361c7
Ben Edwards <hello@benedfit.com>
2020-08-18 01:05:15 +0100

    fix: move body color

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,9 +1,7 @@
 import ContentContainer from '../components/ContentContainer/theme.module.scss'
 import Grid from '../components/Grid/theme.module.scss'
-import SiteContainer from '../components/SiteContainer/theme.module.scss'
 
 export default {
   ContentContainer,
-  Grid,
-  SiteContainer
+  Grid
 }
