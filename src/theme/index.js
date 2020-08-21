a76d8b290c615d50d3bf5c27475e082d96688641
Ben Edwards <hello@benedfit.com>
2020-08-21 09:28:24 +0100

    chore: renames theme files for better debugging

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,5 +1,5 @@
-import ContentContainer from '../components/ContentContainer/theme.module.scss'
-import Grid from '../components/Grid/theme.module.scss'
+import ContentContainer from '../components/ContentContainer/ContentContainer.module.scss'
+import Grid from '../components/Grid/Grid.module.scss'
 
 export default {
   ContentContainer,
