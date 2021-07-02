2c63d055b8b961451c81d9655d290999e340bf57
Ben Edwards <hello@benedfit.com>
2021-07-02 17:24:10 +0100

    fix: link to sitemap.xml

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,25 +1,16 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { config } from '../../site.config'
-import loadConfig from 'next/dist/next-server/server/config'
 
 import logoUrl from '../images/logo.svg'
 
 class DocumentPage extends Document {
   render() {
-    const { sitemap } = loadConfig()
-
     return (
       <Html lang={config.lang}>
         <Head>
           <link rel="icon" href={logoUrl} />
-          {sitemap && (
-            <link
-              rel="sitemap"
-              type="application/xml"
-              href={`/${sitemap.filename}`}
-            />
-          )}
+          <link rel="sitemap" type="application/xml" href="sitemap.xml" />
         </Head>
         <body>
           <Main />
