bbac9276e7ad8c6449e624b61efee1c68de9e28f
Ben Edwards <hello@benedfit.com>
2021-07-05 10:50:40 +0100

    feat: remove custom _document

--- src/components/PageContainer/index.jsx
+++ src/components/PageContainer/index.jsx
@@ -3,6 +3,8 @@ import { node, object } from 'prop-types'
 import { PageContainer as ThemedPageContainer } from '@newhighsco/chipset'
 import { Footer, Header, Meta } from '..'
 
+import logoUrl from '../../images/logo.svg'
+
 const PageContainer = ({ meta, children }) => (
   <ThemedPageContainer
     as="main"
@@ -12,7 +14,20 @@ const PageContainer = ({ meta, children }) => (
     footer={<Footer />}
     gutter
   >
-    <Meta {...meta} />
+    <Meta
+      {...meta}
+      additionalLinkTags={[
+        {
+          rel: 'icon',
+          href: logoUrl
+        },
+        {
+          rel: 'sitemap',
+          type: 'application/xml',
+          href: '/sitemap.xml'
+        }
+      ]}
+    />
     {children}
   </ThemedPageContainer>
 )
