d9dc377db718d26c4fdacff9fe63ead1a8ffd41f
Ben Edwards <hello@benedfit.com>
2023-03-22 14:10:42 +0000

    chore: more debug

--- template/plugins/debug-cache/index.js
+++ template/plugins/debug-cache/index.js
@@ -1,33 +1,15 @@
 const fs = require('fs')
-const path = require('path')
-// const { promisify } = require('util')
-// const writeFile = promisify(fs.writeFile)
-
-// const makeDir = require('make-dir')
-
-const DEFAULT_CACHE_DIR = '.netlify/cache'
 
 module.exports = {
   onPreBuild: async ({ constants, inputs, utils }) => {
-    console.log(DEFAULT_CACHE_DIR)
+    const files = await utils.cache.list()
 
-    const files = fs.readdirSync(path.resolve(DEFAULT_CACHE_DIR))
+    console.log('Cached file count', files.length)
 
     files.forEach(file => {
       const { size } = fs.statSync(file)
 
       console.log(file, size)
     })
-
-    // const { PUBLISH_DIR } = constants
-    // const cacheManifestFileName = inputs.outputFile
-    // const cacheManifestPath = path.join(PUBLISH_DIR, cacheManifestFileName)
-    // console.log('Saving cache file manifest for debugging...')
-    // const files = await utils.cache.list()
-    // await makeDir(PUBLISH_DIR)
-    // await writeFile(cacheManifestPath, JSON.stringify(files, null, 2))
-    // console.log(`Cache file count: ${files.length}`)
-    // console.log(`Cache manifest saved to ${cacheManifestPath}`)
-    // console.log(`Cache manifest output: ${JSON.stringify(files, null, 2)}`)
   }
 }
