bb9f5574f850b419bd4c67fdb3f0cbe5b1e99e75
Ben Edwards <hello@benedfit.com>
2023-03-22 17:03:30 +0000

    chore: recursive

--- template/plugins/debug-cache/index.js
+++ template/plugins/debug-cache/index.js
@@ -1,15 +1,38 @@
 const fs = require('fs')
+const path = require('path')
+
+const getAllFiles = function (dirPath, arrayOfFiles) {
+  const files = fs.readdirSync(dirPath)
+
+  arrayOfFiles = arrayOfFiles || []
+
+  files.forEach(function (file) {
+    if (fs.statSync(dirPath + '/' + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + '/' + file, arrayOfFiles)
+    } else {
+      arrayOfFiles.push(path.join(__dirname, dirPath, '/', file))
+    }
+  })
+
+  return arrayOfFiles
+}
 
 module.exports = {
   onPreBuild: async ({ constants, inputs, utils }) => {
     const files = await utils.cache.list()
 
-    console.log('Cached file count', files.length)
+    console.log('Cached folder count', files.length)
 
     files.forEach(file => {
-      const { size } = fs.statSync(file)
+      console.log(111, file)
 
-      console.log(file, size)
+      const all = getAllFiles(file)
+
+      all.forEach(a => {
+        const { size } = fs.statSync(a)
+
+        console.log(222, a, size)
+      })
     })
   }
 }
