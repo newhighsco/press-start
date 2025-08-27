7e56bf77e22463a73d75dc9dc0d6ff5100f8c6eb
Ben Edwards <hello@benedfit.com>
2025-08-27 16:07:30 +0100

    feat: use `LinkProvider`

--- template/src/components/Header/index.jsx
+++ template/src/components/Header/index.jsx
@@ -4,7 +4,6 @@ import {
   Navigation,
   SmartLink
 } from '@newhighsco/chipset'
-import Link from 'next/link'
 import React from 'react'
 
 import LogoLockup from '~components/LogoLockup'
@@ -17,22 +16,12 @@ const Header = () => (
     <HeaderContainer gutter theme={{ root: styles.root }}>
       <Grid flex valign="middle">
         <Grid.Item className={styles.logo}>
-          <Link href="/" passHref legacyBehavior>
-            <SmartLink>
-              <LogoLockup />
-            </SmartLink>
-          </Link>
+          <SmartLink href="/">
+            <LogoLockup />
+          </SmartLink>
         </Grid.Item>
         <Grid.Item className={styles.links}>
-          <Navigation
-            links={header.links}
-            renderLink={({ href, children, ...rest }) => (
-              <Link href={href} passHref legacyBehavior>
-                <SmartLink {...rest}>{children}</SmartLink>
-              </Link>
-            )}
-            theme={{ link: styles.link }}
-          />
+          <Navigation links={header.links} theme={{ link: styles.link }} />
         </Grid.Item>
       </Grid>
     </HeaderContainer>
