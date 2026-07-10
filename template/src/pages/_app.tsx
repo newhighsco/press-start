e98e868acfebfb9e495c63cdb9e61ab75c9c1e77
Ben Edwards <hello@benedfit.com>
2026-07-10 15:40:47 +0100

    feat(fonts): use mixins and functions to preload

--- template/src/pages/_app.tsx
+++ template/src/pages/_app.tsx
@@ -5,10 +5,11 @@ import { type AppProps } from 'next/app'
 import React, { type FC } from 'react'
 
 import config from '~config'
+import { default as fonts } from '~styles/font-faces.module.scss'
 import theme from '~theme'
 
 const App: FC<AppProps> = props => (
-  <AppPage {...props} theme={theme} config={config} />
+  <AppPage {...props} theme={theme} config={config} fonts={fonts} />
 )
 
 export default App
